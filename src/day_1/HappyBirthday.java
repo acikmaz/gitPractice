1fcf98b598851710cc6a3db9a521a9d911db7478
acikmaz <64147526+acikmaz@users.noreply.github.com>
2020-04-28 15:02:40 -0500

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -5,5 +5,8 @@ public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("RANA");
         System.out.println("BERA");
+        
+                System.out.println("seda");
+        System.out.println("Ali");
     }
 }
